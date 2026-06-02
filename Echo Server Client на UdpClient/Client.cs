922a92337e7a37e3bb44947380d9206d943f5e90
Popovyc-h <popovichtanja86@gmail.com>
2026-06-02 23:08:52 +0300

    Create Client.cs

---
+++ Echo Server Client на UdpClient/Client.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new UdpClient();
+        var serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5005);
+        var buffer = new byte[1024];
+
+        while (true)
+        {
+            Console.Write("\nВведіть повідомлення: ");
+            string message = Console.ReadLine();
+
+            var messageByte = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(messageByte, serverEndpoint);
+
+            var result = await client.ReceiveAsync();
+
+            string str = Encoding.UTF8.GetString(result.Buffer);
+
+            Console.WriteLine($">> Echo: {str}");
+        }
+    }
+}
